4024d6fb65c84e81948f30d4b19276d79d01044e
Juan-010 <juanchijujuy5@gmail.com>
2021-11-10 05:50:08 -0300

    añade programa principal

--- sketches/buttonMl/buttonMl.ino
+++ sketches/buttonMl/buttonMl.ino
@@ -9,13 +9,17 @@
 Adafruit_SSD1306 oled(ANCHO, ALTO, &Wire, OLED_RESET);
 
 //Valve
-#define BUTTONPIN 3 
-#define MLPERS 50
+#define BUTTONPIN 4 
+#define RELAYPIN 7
+#define BUZZERPIN 8
+#define MLPERS 100
+#define HOUR 3600000
+
 long lastMillis;
 void setup() {
 	pinMode(BUTTONPIN, INPUT_PULLUP);
-	pinMode(13, OUTPUT);
-	pinMode(8, OUTPUT);
+	pinMode(RELAYPIN, OUTPUT);
+	pinMode(BUZZERPIN, OUTPUT);
 	Wire.begin();
 	oled.begin(SSD1306_SWITCHCAPVCC, 0x3C);
 	lastMillis = millis();
@@ -23,28 +27,49 @@ void setup() {
 
 void loop() {
 	static unsigned long mL = 0;
+	static unsigned int horas = 0;
+	
 	//oled stuff
 	oled.setTextColor(WHITE);
 	oled.setCursor(0, 30);
 	oled.setTextSize(2);
-	if(!digitalRead(BUTTONPIN)){
-		digitalWrite(13, 1);
+	
+
+	if(!digitalRead(BUTTONPIN)){	
+		digitalWrite(RELAYPIN, 1);
 		delay(250);
-		mL += MLPERS;
+		mL += MLPERS/4;
 		oled.clearDisplay();
 		oled.print(mL);
 		oled.print(" ml.");
 		oled.display();
 	}
+
 	else
-		digitalWrite(13,0);
-	if((millis() - lastMillis) >= 30000){
+		digitalWrite(RELAYPIN, 0);
+	
+	
+	// Reset Handler
+	if((millis() - lastMillis) >= HOUR){
 		lastMillis = millis();
-		tone(8, 500, 200);
+		horas++;
+	}
+
+	if(horas >= 24){
+		//lastMl = mL;
 		mL = 0;
+		
+		tone(8, 500, 200);
+		oled.clearDisplay();
+		oled.print("NUEVO DIA");
+		oled.display();
+		delay(200);
+		
 		oled.clearDisplay();
 		oled.print(mL);
 		oled.print(" ml.");
 		oled.display();
+
+		horas = 0;
 	}
 }
