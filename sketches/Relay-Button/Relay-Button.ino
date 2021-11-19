72725f019fd5a19094ada40fb3c45b00bbca5075
Juan-010 <juanchijujuy5@gmail.com>
2021-11-19 06:49:27 -0300

    Commit previo a presentacion

--- sketches/Relay-Button/Relay-Button.ino
+++
@@ -1,9 +0,0 @@
-#define RELAYPIN 7
-#define BUTTONPIN 8
-void setup() {
-	pinMode(RELAYPIN, OUTPUT);
-	pinMode(BUTTONPIN, INPUT_PULLUP);
-}
-void loop() {
-	digitalWrite(RELAYPIN,!digitalRead(BUTTONPIN));
-}
